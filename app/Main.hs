92bccf3c9cc0c14db63a404ddc3addfbdb9e0cc1
Guilherme <guipython2001@gmail.com>
2022-07-15 15:05:10 -0300

    refct: Atualizacao de testes e imports na funcao main

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,23 @@
+{-# LANGUAGE PolyKinds #-}
+
 module Main where
 
-import Spies (Spy(..), canDoWaterMissions, specifiedSpies)
-import Repository (find)
+import Common.TypeOperators (eval)
+import Domain.Spies.Specifications.CanDo (canDoWater)
+import Domain.Spies.Specifications.CanPilot (canDriveCar, canPilotBoat)
+import Domain.Spies.Specifications.SpySpecification (SpecifiedSpy, specifiedSpies, specifiedSpy)
+import Domain.Spies.Spy (Spy (..))
 
-spies :: [Spy]
-spies = [Spy "Marcos" 23, Spy "Marcos" 21, Spy "Jorge" 18]
+spy = Spy "C-137" 58
+
+spies = [Spy "George" 15, Spy "Thomas" 18, Spy "Mohamed" 10]
+
+valid specified = canDriveCar specified >>= canPilotBoat >>= canDoWater
 
 main :: IO ()
-main = print (find canDoWaterMissions (specifiedSpies spies))
+main =
+  print $
+    name spy
+      ++ if eval $ valid $ specifiedSpy spy
+        then " esta apto para missoes em agua"
+        else "nao esta apto para missoes em agua"
