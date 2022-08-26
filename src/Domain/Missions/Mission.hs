633a4670a01373f4f79ca069a12a5be7024f22b3
Guilherme Rocha <GuiPython2001@gmail.com>
2022-08-25 21:54:09 -0300

    refact: Criando funções eliminadoras, renomeando tipo existencial e exportando-o, escondendo contrutores e exportando smart-constructors

--- src/Domain/Missions/Mission.hs
+++ src/Domain/Missions/Mission.hs
@@ -3,12 +3,21 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-module Domain.Missions.Mission (mkAquaticMission, mkAeroMission, mkAeroAquaticMission, mkInvalidMission, transition, execute, complete, value, updateValue, Mission(..), MissionInfo(..), State(..)) where
+{-# LANGUAGE RankNTypes #-}
+module Domain.Missions.Mission (
+  mkAquaticMission,
+  mkAeroMission, 
+  mkAeroAquaticMission, 
+  mkInvalidMission, 
+  transition, execute, complete,
+  value, updateValue,
+  withSomeMissionInfo, withSomeMissionState,
+  Mission, State(..), MissionInfo(..)) where
 
 import Data.Kind (Type)
 import Common.Operators (Satisfied, Expression(Spec), And)
-import Domain.Spies.Specifications.SpySpecification
-    ( SpecifiedSpy (SpecifiedSpy), Specs(Do), CanDo(AquaticMission, AeroMission) )
+import Domain.Spies.Specifications
+    ( SpecifiedSpy, Specs(Do), CanDo(AquaticMission, AeroMission), withSpy )
 import Domain.Spies.Spy(Spy (name))
 
 data State = Pending | Execution | Executed | Invalided deriving Show
@@ -31,21 +40,29 @@ data MissionInfo spy value where
     _value :: Double
   } -> MissionInfo spy value
 
-  MkInvalidMission :: MissionInfo spy value
+  InvalidMissionInfo :: MissionInfo spy value
 
 
-type Mission :: Type -> State -> Type
-data Mission info state where
-  MkMission :: MissionInfo spy value -> State -> Mission info state
+type ExMission :: Type -> State -> Type
+data ExMission info state where
+  MkMission :: MissionInfo spy value -> State -> ExMission info state
 
-instance Show (Mission info state) where
-  show (MkMission MkInvalidMission state) = "Invalid mission"
-  show (MkMission (AquaticInfo (SpecifiedSpy spy) value) state) = "Aquatic Mission for " ++ name spy ++ " " ++ show state
-  show (MkMission (AeroInfo (SpecifiedSpy spy) value) state) = "Aero Mission for " ++ name spy ++ " " ++ show state
-  show (MkMission (AeroAquaticInfo (SpecifiedSpy spy) value) state) = "Aero-Aquatic Mission for " ++ name spy ++ " " ++ show state
+instance Show (ExMission info state) where
+  show (MkMission InvalidMissionInfo state) = "Invalid mission"
+  show (MkMission (AquaticInfo specifiedSpy value) state) = "Aquatic Mission for " ++ withSpy specifiedSpy name ++ " " ++ show state
+  show (MkMission (AeroInfo specifiedSpy value) state) = "Aero Mission for " ++ withSpy specifiedSpy name ++ " " ++ show state
+  show (MkMission (AeroAquaticInfo specifiedSpy value) state) = "Aero-Aquatic Mission for " ++ withSpy specifiedSpy name ++ " " ++ show state
+
+withSomeMissionInfo :: ExMission info state -> (forall s v. MissionInfo s v -> return) -> return
+withSomeMissionInfo (MkMission info state) f = f info
+
+withSomeMissionState :: ExMission info state -> (State -> return) -> return
+withSomeMissionState (MkMission info state) f = f state
+
+type Mission = ExMission
 
 mkInvalidMission :: Mission (MissionInfo Spy value) Pending
-mkInvalidMission = MkMission MkInvalidMission Pending
+mkInvalidMission = MkMission InvalidMissionInfo Pending
 
 mkAquaticMission :: (Satisfied (Spec (Do AquaticMission)) specs) => SpecifiedSpy specs -> Double -> Mission(MissionInfo Spy Double) Pending
 mkAquaticMission specified value = MkMission (AquaticInfo specified value) Pending
@@ -56,11 +73,11 @@ mkAeroMission specified value = MkMission (AeroInfo specified value) Pending
 mkAeroAquaticMission :: (Satisfied (And (Spec (Do AeroMission)) (Spec (Do AquaticMission))) specs) => SpecifiedSpy specs -> Double -> Mission(MissionInfo Spy Double) Pending
 mkAeroAquaticMission specified value = MkMission (AeroAquaticInfo specified value) Pending
 
-value :: Mission (MissionInfo Spy Double) state -> Double
+value :: Mission (MissionInfo Spy Double) Pending -> Double
 value (MkMission (AeroInfo _ v) _) = v
 value (MkMission (AquaticInfo _ v) _) = v
 value (MkMission (AeroAquaticInfo spy v) _) = v 
-value (MkMission MkInvalidMission _) = 0 
+value (MkMission InvalidMissionInfo _) = 0 
 
 updateValue :: Mission (MissionInfo Spy Double) Pending -> Double -> Mission (MissionInfo Spy Double) Pending
 updateValue (MkMission (AeroInfo spy _) _) value = mkAeroMission spy value
