bae2e3d3e8d5922ac0c83764197e9ad2ef995930
Guilherme Rocha <GuiPython2001@gmail.com>
2022-08-25 21:50:10 -0300

    refact: escondendo contrutores, exportando somente smart-constructors, criando especificações no mesmo arquivo e criação de função eliminadora

--- src/Domain/Missions/Specifications.hs
+++ src/Domain/Missions/Specifications.hs
@@ -2,45 +2,53 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
-module Domain.Missions.Specifications (isAquatic, isAero, isAeroAquatic, specifiedMission, specifiedMissions, isPending, SpecifiedMission(..), Specs(..)) where
+{-# LANGUAGE LambdaCase #-}
+module Domain.Missions.Specifications (isAquatic, isAero, isAeroAquatic, specifiedMission, specifiedMissions, isPending, SpecifiedMission, Specs(..)) where
 
 import Data.Kind ( Type, Constraint )
-import Domain.Missions.Mission (Mission(..), MissionInfo(..), State(..))
+import Domain.Missions.Mission (Mission, MissionInfo(..), State(..), withSomeMissionState, withSomeMissionInfo, mkAquaticMission, mkAeroAquaticMission, mkAeroMission, mkInvalidMission)
+import Domain.Spies.Specifications (CanDo(AquaticMission))
 
 data Specs = IsAquatic | IsAero | IsAeroAquatic deriving Show
 
 type SpecifiedMission :: [Specs] -> State -> Type
 data SpecifiedMission specifications state where 
-  SpecifiedMission :: Mission (MissionInfo spy value) state -> SpecifiedMission specifications state
+  SpecifiedMission :: Mission info state -> SpecifiedMission specifications state
 
 instance Show (SpecifiedMission specifications state) where
   show (SpecifiedMission mission) = show mission
 
-specifiedMission :: Mission (MissionInfo spy value) state -> SpecifiedMission '[] state
+specifiedMission :: Mission info state -> SpecifiedMission '[] state
 specifiedMission = SpecifiedMission
 
-specifiedMissions :: [Mission (MissionInfo spy value) state] -> [SpecifiedMission '[] state]
+specifiedMissions :: [Mission info state] -> [SpecifiedMission '[] state]
 specifiedMissions = foldr (\ spy -> (<>) [specifiedMission spy]) []
 
 isAquatic :: SpecifiedMission specs state -> Maybe(SpecifiedMission (IsAquatic ': specs) state)
-isAquatic (SpecifiedMission mission) = case mission of 
-  MkMission info state -> case info of
-      AquaticInfo _ _ -> Just $ SpecifiedMission mission
-      _ -> Nothing
+isAquatic (SpecifiedMission mission) = withSomeMissionInfo mission (\case
+    AquaticInfo _ _ -> Just $ SpecifiedMission mission
+    _ -> Nothing
+  )
 
 isAero :: SpecifiedMission specs state -> Maybe(SpecifiedMission (IsAero ': specs) state)
-isAero (SpecifiedMission mission) = case mission of 
-    MkMission info state -> case info of
-      AeroInfo _ _ -> Just $ SpecifiedMission mission
-      _ -> Nothing
+isAero (SpecifiedMission mission) = withSomeMissionInfo mission (\case
+    AeroInfo _ _ -> Just $ SpecifiedMission mission
+    _ -> Nothing
+  )
 
 isAeroAquatic :: SpecifiedMission specs state -> Maybe(SpecifiedMission (IsAeroAquatic ': specs) state)
-isAeroAquatic (SpecifiedMission mission) = case mission of 
-    MkMission info state -> case info of
-      AeroAquaticInfo _ _ -> Just $ SpecifiedMission mission
-      _ -> Nothing 
+isAeroAquatic (SpecifiedMission mission) = withSomeMissionInfo mission (\case
+    AeroAquaticInfo _ _ -> Just $ SpecifiedMission mission
+    _ -> Nothing
+  )
 
 isPending :: SpecifiedMission specs state -> Maybe(SpecifiedMission specs Pending)
-isPending (SpecifiedMission mission) = case mission of 
-  (MkMission info Pending) -> Just $ SpecifiedMission (MkMission info Pending)
-  (MkMission _ _) -> Nothing
+isPending (SpecifiedMission mission) = withSomeMissionState mission (\case 
+  Pending -> withSomeMissionInfo mission (\case
+    AeroAquaticInfo spy value -> Just $ SpecifiedMission $ mkAeroAquaticMission spy value
+    AquaticInfo spy value -> Just $ SpecifiedMission $ mkAquaticMission spy value
+    AeroInfo spy value -> Just $ SpecifiedMission $ mkAeroMission spy value
+    InvalidMissionInfo -> Just $ SpecifiedMission mkInvalidMission
+    )
+  _ -> Nothing
+  )
